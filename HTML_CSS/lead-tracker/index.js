73828fd153652f343339d332498675137ab9e575
Siddami <dharmmy621@gmail.com>
2025-05-20 16:40:48 +0100

    feat: add firebase database management in script

--- HTML_CSS/lead-tracker/index.js
+++ HTML_CSS/lead-tracker/index.js
@@ -1,31 +1,55 @@
-import {initializeApp} from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+  onValue,
+  remove,
+} from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
 
-const inputEl = document.getElementById("input-el")
-const inputBtn = document.getElementById("input-btn")
-const ulEl = document.getElementById("ul-el")
-const deleteBtn = document.getElementById("delete-btn")
+const firebaseConfig = {
+  databaseURL: "https://leads-generator-app-e355f-default-rtdb.firebaseio.com/",
+};
 
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+const leadRef = ref(db, "leads");
 
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
 
 function render(leads) {
-    let listItems = ""
-    for (let i = 0; i < leads.length; i++) {
-        listItems += `
+  let listItems = "";
+  for (let i = 0; i < leads.length; i++) {
+    listItems += `
             <li>
                 <a target='_blank' href='${leads[i]}'>
                     ${leads[i]}
                 </a>
             </li>
-        `
-    }
-    ulEl.innerHTML = listItems
+        `;
+  }
+  ulEl.innerHTML = listItems;
 }
 
-deleteBtn.addEventListener("dblclick", function() {
-    render(myLeads)
-})
+onValue(leadRef, (snapshot) => {
+  const snapshotExists = snapshot.exists();
 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    render(myLeads)
-})
+  if (snapshotExists) {
+    const data = snapshot.val();
+    const myLeads = Object.values(data);
+    render(myLeads);
+  }
+});
+
+deleteBtn.addEventListener("dblclick", function () {
+  remove(leadRef);
+  ulEl.innerHTML = "";
+});
+
+inputBtn.addEventListener("click", function () {
+  push(leadRef, inputEl.value);
+  inputEl.value = "";
+});
